41bd34e65732ea078dfade1318cc1683c3c4119f
2b3q <2bb3qq@gmail.com>
2017-10-13 09:38:17 +0300

    add user session behavior. Testing vars

--- routes/beers.js
+++ routes/beers.js
@@ -8,9 +8,9 @@ router.get('/', function(req, res, next){
   console.log('Req.query: '+req.query.query);
   var searchTerm = req.query.query || 'STOUT';
   elastic.search(searchTerm, function(data) {
-    req.session.userName = 'Anonimous';
-    res.cookie('signed_token', 'omnomnom', { signed: true });
-    res.render('beers', { title: 'Express +  Bootstrap + Pug + ElasticSearch', results: data, query: searchTerm });
+    // req.session.userName = 'Anonimous';
+    // res.cookie('signed_token', 'omnomnom', { signed: true });
+    res.render('beers', { title: 'Express +  Bootstrap + Pug + ElasticSearch',user: req.session.username, results: data, query: searchTerm });
   });
 });
 
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  req.session.userName = 'Anonimous';
+  req.session.userName = 'Anonimous'; // set user session object
   res.cookie('signed_token', 'omnomnom', { signed: true });
   // req.flash('notify', 'This is a test notification.')
   res.render('index', { title: 'Express PUG Bootstrap' });
