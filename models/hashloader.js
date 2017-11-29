ca208dd02bc6ee1fda2295a5aa1c2179672e858e
root <root@beeroteka.ru>
2017-11-29 18:40:44 +0300

    2 arrays

--- models/hashloader.js
+++ models/hashloader.js
@@ -5,6 +5,9 @@ var es_client = require('../libs/elasticsearch'), // require ES module
     apivoModel = require('./apivoModel'), // Create Apivo schema model
     query = require('./dsl');
 
+var arr1 = [],
+    arr2 = [];
+
 apivoModel.find({}, function(err, docs){
   if(err) log.error(`ERROR while getting docs from mongo: "${err}"`)
   console.log(docs);
@@ -17,6 +20,7 @@ apivoModel.find({}, function(err, docs){
    - PUSH to mongo collction AP (AP_arr.forEach LOOKUP in BA index)
 */
 var hashToLoad = {}; // Common beer Hash to LOAD
+var hashToLoad2 = {}; // Common beer Hash to LOAD
 
 var isFloat = n => n === +n && n !== (n|0),
     isInteger = n => n === +n && n === (n|0)
@@ -35,7 +39,7 @@ function getApDocs(){
 
 // searh Matches in BA index
 function searchBaMatches(ApDocs){
-  ApDocs.forEach(function(item, i){
+  ApDocs.forEach(function(item){
     // Create AP properties
     hashToLoad.ap_beer = item._source.beer || '';
     hashToLoad.ap_orig_beer_name = item._source['Название'];
@@ -55,6 +59,7 @@ function searchBaMatches(ApDocs){
     hashToLoad.ap_taste = item._source['Вкусовые оттенки'] || '';
     hashToLoad.ap_desc = item._source['desc'] || {};
     // console.log(`hashToLoad = ${JSON.stringify(hashToLoad)}`);
+    arr1.push(hashToLoad);
 
     // create BA query object
     let query_object = {
@@ -65,46 +70,52 @@ function searchBaMatches(ApDocs){
       country: hashToLoad.country_obj.name,
       abv: (isInteger(hashToLoad.ap_abv) || isFloat(hashToLoad.ap_abv)) ? hashToLoad.ap_abv : 0
     }
-
     // console.log(config.color.white+JSON.stringify(query_object));
-
     // LookUP beer in BA from AP properties
     es_client.client.search(query.getBaFromAp(query_object))
       .then(function(resp){
         if( resp.hits.hits.length > 0 ) {
           // console.log(config.color.yellow+JSON.stringify(resp.hits.hits[0]));
-          hashToLoad.ba_score =       resp.hits.hits[0]._score;
-          hashToLoad.ba_beer_score =  resp.hits.hits[0]._source.score;
-          hashToLoad.ba_img =         resp.hits.hits[0]._source.img;
-          hashToLoad.ba_title =       resp.hits.hits[0]._source.title;
-          hashToLoad.ba_url =         resp.hits.hits[0]._source.url;
-          hashToLoad.ba_ratings =     resp.hits.hits[0]._source.Ratings;
-          hashToLoad.ba_reviews =     resp.hits.hits[0]._source.Reviews;
-          hashToLoad.ba_abv =         resp.hits.hits[0]._source.abv;
-          hashToLoad.ba_brewary =     resp.hits.hits[0]._source.brewary;
-          hashToLoad.ba_beer =        resp.hits.hits[0]._source.beer;
-          hashToLoad.ba_style =       resp.hits.hits[0]._source.style;
-          hashToLoad.ba_category =    resp.hits.hits[0]._source.category;
-        }
-        /*
-          INSERTING hashToLoad
-        */
-        // console.log(config.color.yellow+JSON.stringify(hashToLoad));
-        // Create an instance of model apivoModel
-        if( hashToLoad.ba_score !== undefined && hashToLoad.ba_abv === hashToLoad.ap_abv ) {
-          var hashToLoad_instance = new apivoModel(hashToLoad);
-          // Save hashToLoad_instance, passing a callback
-          hashToLoad_instance.save(function (err) {
-            if (err) log.error(`ERROR while INSERTING data: "${err}"`);
-            // saved!
-          });
-        }
+          hashToLoad2.ba_score =       resp.hits.hits[0]._score; // Do not use iterator [i] => caz first object is most relevanted response
+          hashToLoad2.ba_beer_score =  resp.hits.hits[0]._source.score;
+          hashToLoad2.ba_img =         resp.hits.hits[0]._source.img;
+          hashToLoad2.ba_title =       resp.hits.hits[0]._source.title;
+          hashToLoad2.ba_url =         resp.hits.hits[0]._source.url;
+          hashToLoad2.ba_ratings =     resp.hits.hits[0]._source.Ratings;
+          hashToLoad2.ba_reviews =     resp.hits.hits[0]._source.Reviews;
+          hashToLoad2.ba_abv =         resp.hits.hits[0]._source.abv;
+          hashToLoad2.ba_brewary =     resp.hits.hits[0]._source.brewary;
+          hashToLoad2.ba_beer =        resp.hits.hits[0]._source.beer;
+          hashToLoad2.ba_style =       resp.hits.hits[0]._source.style;
+          hashToLoad2.ba_category =    resp.hits.hits[0]._source.category;
 
+          arr2.push(hashToLoad2);
+          /*
+          INSERTING hashToLoad
+          */
+          console.log(config.color.yellow+JSON.stringify(hashToLoad2));
+          // Create an instance of model apivoModel
+          // var hashToLoad_instance = new apivoModel(hashToLoad2);
+          // // Save hashToLoad_instance, passing a callback
+          // hashToLoad_instance.save(function (err) {
+          //   if (err) log.error(`ERROR while INSERTING data: "${err}"`);
+          //   // saved!
+          // });
+        }
       }, function(err){
         log.error(err.message)
       })
+    })
+    console.log(`array1 length: "${arr1.length}"`);
+    console.log(`array2 length: "${arr2.length}"`);
 
-  })
+    setTimeout(function(){
+      console.log(`${config.color.white} array3 length: "${arr2.length}"`);
+    }, 1000)
+
+
+
+
 }
 
 var LoadHashes = function(){
