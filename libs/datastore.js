54d5030ac80a699ab0f7b825f8447731da0427b7
root <root@beeroteka.ru>
2017-11-29 15:33:51 +0300

    bullshit (async)

--- libs/datastore.js
+++ libs/datastore.js
@@ -6,9 +6,12 @@ var mongoose = require('mongoose'),
 /* Run specific mongodb
 == Docker ==
 docker run -d --name mongo -p 127.0.0.1:27017:27017 -p 127.0.0.1:28017:28017 -e MONGODB_USER="beeroteka" -e MONGODB_DATABASE="beeroteka" -e MONGODB_PASS="Dr1nkM0reBeeR" tutum/mongodb
+docker run -d --name mongo -p 127.0.0.1:27017:27017 -p 127.0.0.1:28017:28017 -e MONGODB_PASS="Dr1nkM0reBeeR" tutum/mongodb
 
 == HINTS ==
 Mongoose (ODM - Object Data Model): Mongoose is a MongoDB object modeling tool designed to work in an asynchronous environment.
+ sudo ssh -f -N -L 9200:localhost:9200 beeroteka (forward ES 9200 port to client host)
+ sudo ssh -f -N -L 28017:127.0.0.1:28017 root@beeroteka (forward Mongo Web console to client host)
 */
 
 var initMongo = function(){
--- models/hashloader.js
+++ models/hashloader.js
@@ -2,7 +2,13 @@ var es_client = require('../libs/elasticsearch'), // require ES module
     log = require('../libs/log')(module),
     config = require('../config/config'),
     dict = require('./dict'), // rus > eng dict
+    apivoModel = require('./apivoModel'), // Create Apivo schema model
     query = require('./dsl');
+
+apivoModel.find({}, function(err, docs){
+  if(err) log.error(`ERROR while getting docs from mongo: "${err}"`)
+  console.log(docs);
+})
 
 /** TODO
    - add mongoose API
@@ -60,12 +66,40 @@ function searchBaMatches(ApDocs){
       abv: (isInteger(hashToLoad.ap_abv) || isFloat(hashToLoad.ap_abv)) ? hashToLoad.ap_abv : 0
     }
 
+    // console.log(config.color.white+JSON.stringify(query_object));
+
     // LookUP beer in BA from AP properties
     es_client.client.search(query.getBaFromAp(query_object))
       .then(function(resp){
-        console.log(config.color.yellow+JSON.stringify(hashToLoad));
-        console.log(config.color.white+JSON.stringify(query_object));
-        console.log(config.color.cyan+JSON.stringify(resp.hits.hits));
+        if( resp.hits.hits.length > 0 ) {
+          // console.log(config.color.yellow+JSON.stringify(resp.hits.hits[0]));
+          hashToLoad.ba_score =       resp.hits.hits[0]._score;
+          hashToLoad.ba_beer_score =  resp.hits.hits[0]._source.score;
+          hashToLoad.ba_img =         resp.hits.hits[0]._source.img;
+          hashToLoad.ba_title =       resp.hits.hits[0]._source.title;
+          hashToLoad.ba_url =         resp.hits.hits[0]._source.url;
+          hashToLoad.ba_ratings =     resp.hits.hits[0]._source.Ratings;
+          hashToLoad.ba_reviews =     resp.hits.hits[0]._source.Reviews;
+          hashToLoad.ba_abv =         resp.hits.hits[0]._source.abv;
+          hashToLoad.ba_brewary =     resp.hits.hits[0]._source.brewary;
+          hashToLoad.ba_beer =        resp.hits.hits[0]._source.beer;
+          hashToLoad.ba_style =       resp.hits.hits[0]._source.style;
+          hashToLoad.ba_category =    resp.hits.hits[0]._source.category;
+        }
+        /*
+          INSERTING hashToLoad
+        */
+        // console.log(config.color.yellow+JSON.stringify(hashToLoad));
+        // Create an instance of model apivoModel
+        if( hashToLoad.ba_score !== undefined && hashToLoad.ba_abv === hashToLoad.ap_abv ) {
+          var hashToLoad_instance = new apivoModel(hashToLoad);
+          // Save hashToLoad_instance, passing a callback
+          hashToLoad_instance.save(function (err) {
+            if (err) log.error(`ERROR while INSERTING data: "${err}"`);
+            // saved!
+          });
+        }
+
       }, function(err){
         log.error(err.message)
       })
