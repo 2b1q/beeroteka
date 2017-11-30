77a139e6d5e8400104a8bafe2c4409de0dcc9937
2b3q <2bb3qq@gmail.com>
2017-11-30 09:27:17 +0300

    suburban train commit

--- models/hashloader.js
+++ models/hashloader.js
@@ -5,8 +5,8 @@ var es_client = require('../libs/elasticsearch'), // require ES module
     apivoModel = require('./apivoModel'), // Create Apivo schema model
     query = require('./dsl');
 
-var arr1 = [],
-    arr2 = [];
+var ap_arr = [], // apivo search result array from ES
+    ba_arr = []; // BeerAdvocate search result array from ES
 
 apivoModel.find({}, function(err, docs){
   if(err) log.error(`ERROR while getting docs from mongo: "${err}"`)
@@ -14,13 +14,12 @@ apivoModel.find({}, function(err, docs){
 })
 
 /** TODO
-   - add mongoose API
    - add search from BA in AP
    - PUSH to mongo collction BA (BA_arr.forEach LOOKUP in AP index)
    - PUSH to mongo collction AP (AP_arr.forEach LOOKUP in BA index)
 */
-var hashToLoad = {}; // Common beer Hash to LOAD
-var hashToLoad2 = {}; // Common beer Hash to LOAD
+var ap_json = {}; // One matched AP JSON result from ES to LOAD
+var ba_json = {}; // One matched BA JSON result from ES to LOAD
 
 var isFloat = n => n === +n && n !== (n|0),
     isInteger = n => n === +n && n === (n|0)
@@ -41,34 +40,34 @@ function getApDocs(){
 function searchBaMatches(ApDocs){
   ApDocs.forEach(function(item){
     // Create AP properties
-    hashToLoad.ap_beer = item._source.beer || '';
-    hashToLoad.ap_orig_beer_name = item._source['Название'];
-    hashToLoad.ap_brewary = item._source.brewary || '';
-    hashToLoad.ap_style = item._source['Вид пива'] || '';
-    hashToLoad.ap_country = item._source['Страна'];
-    hashToLoad.country_obj = dict.getCountry(item._source['Страна']) || {};
-    hashToLoad.ap_abv = item._source.abv;
-    hashToLoad.ap_vol = item._source['Объем'] || '';
-    hashToLoad.ap_density = item._source['density'] || '';
-    hashToLoad.ap_tara = item._source['Тара'] || '';
-    hashToLoad.ap_type = item._source['Тип брожения'] || '';
-    hashToLoad.ap_price_str = item._source['Цена'] || '';
-    hashToLoad.ap_price_num = item._source['price'] || '';
-    hashToLoad.ap_composition = item._source['Состав'] || '';
-    hashToLoad.ap_url = item._source['url'] || '';
-    hashToLoad.ap_taste = item._source['Вкусовые оттенки'] || '';
-    hashToLoad.ap_desc = item._source['desc'] || {};
-    // console.log(`hashToLoad = ${JSON.stringify(hashToLoad)}`);
-    arr1.push(hashToLoad);
+    ap_json.ap_beer = item._source.beer || '';
+    ap_json.ap_orig_beer_name = item._source['Название'];
+    ap_json.ap_brewary = item._source.brewary || '';
+    ap_json.ap_style = item._source['Вид пива'] || '';
+    ap_json.ap_country = item._source['Страна'];
+    ap_json.country_obj = dict.getCountry(item._source['Страна']) || {};
+    ap_json.ap_abv = item._source.abv;
+    ap_json.ap_vol = item._source['Объем'] || '';
+    ap_json.ap_density = item._source['density'] || '';
+    ap_json.ap_tara = item._source['Тара'] || '';
+    ap_json.ap_type = item._source['Тип брожения'] || '';
+    ap_json.ap_price_str = item._source['Цена'] || '';
+    ap_json.ap_price_num = item._source['price'] || '';
+    ap_json.ap_composition = item._source['Состав'] || '';
+    ap_json.ap_url = item._source['url'] || '';
+    ap_json.ap_taste = item._source['Вкусовые оттенки'] || '';
+    ap_json.ap_desc = item._source['desc'] || {};
+    console.log(`${config.color.white}APIVO JSON: ${JSON.stringify(ap_json)}`);
+    ap_arr.push(ap_json);
 
     // create BA query object
     let query_object = {
-      beer: hashToLoad.ap_beer,
-      beer_orig_name: hashToLoad.ap_orig_beer_name,
-      brewary: hashToLoad.ap_brewary,
-      style: hashToLoad.ap_style.replace(/[^a-zA-Z0-9 ]/g, ''),
-      country: hashToLoad.country_obj.name,
-      abv: (isInteger(hashToLoad.ap_abv) || isFloat(hashToLoad.ap_abv)) ? hashToLoad.ap_abv : 0
+      beer: ap_json.ap_beer,
+      beer_orig_name: ap_json.ap_orig_beer_name,
+      brewary: ap_json.ap_brewary,
+      style: ap_json.ap_style.replace(/[^a-zA-Z0-9 ]/g, ''),
+      country: ap_json.country_obj.name,
+      abv: (isInteger(ap_json.ap_abv) || isFloat(ap_json.ap_abv)) ? ap_json.ap_abv : 0
     }
     // console.log(config.color.white+JSON.stringify(query_object));
     // LookUP beer in BA from AP properties
@@ -76,24 +75,24 @@ function searchBaMatches(ApDocs){
       .then(function(resp){
         if( resp.hits.hits.length > 0 ) {
           // console.log(config.color.yellow+JSON.stringify(resp.hits.hits[0]));
-          hashToLoad2.ba_score =       resp.hits.hits[0]._score; // Do not use iterator [i] => caz first object is most relevanted response
-          hashToLoad2.ba_beer_score =  resp.hits.hits[0]._source.score;
-          hashToLoad2.ba_img =         resp.hits.hits[0]._source.img;
-          hashToLoad2.ba_title =       resp.hits.hits[0]._source.title;
-          hashToLoad2.ba_url =         resp.hits.hits[0]._source.url;
-          hashToLoad2.ba_ratings =     resp.hits.hits[0]._source.Ratings;
-          hashToLoad2.ba_reviews =     resp.hits.hits[0]._source.Reviews;
-          hashToLoad2.ba_abv =         resp.hits.hits[0]._source.abv;
-          hashToLoad2.ba_brewary =     resp.hits.hits[0]._source.brewary;
-          hashToLoad2.ba_beer =        resp.hits.hits[0]._source.beer;
-          hashToLoad2.ba_style =       resp.hits.hits[0]._source.style;
-          hashToLoad2.ba_category =    resp.hits.hits[0]._source.category;
+          ba_json.ba_score =       resp.hits.hits[0]._score; // Do not use iterator [i] => caz first object is most relevanted response
+          ba_json.ba_beer_score =  resp.hits.hits[0]._source.score;
+          ba_json.ba_img =         resp.hits.hits[0]._source.img;
+          ba_json.ba_title =       resp.hits.hits[0]._source.title;
+          ba_json.ba_url =         resp.hits.hits[0]._source.url;
+          ba_json.ba_ratings =     resp.hits.hits[0]._source.Ratings;
+          ba_json.ba_reviews =     resp.hits.hits[0]._source.Reviews;
+          ba_json.ba_abv =         resp.hits.hits[0]._source.abv;
+          ba_json.ba_brewary =     resp.hits.hits[0]._source.brewary;
+          ba_json.ba_beer =        resp.hits.hits[0]._source.beer;
+          ba_json.ba_style =       resp.hits.hits[0]._source.style;
+          ba_json.ba_category =    resp.hits.hits[0]._source.category;
 
-          arr2.push(hashToLoad2);
+          ba_arr.push(ba_json);
           /*
           INSERTING hashToLoad
           */
-          console.log(config.color.yellow+JSON.stringify(hashToLoad2));
+          console.log(`${config.color.yellow}BA JSON: ${JSON.stringify(ba_json)}`);
           // Create an instance of model apivoModel
           // var hashToLoad_instance = new apivoModel(hashToLoad2);
           // // Save hashToLoad_instance, passing a callback
@@ -106,15 +105,12 @@ function searchBaMatches(ApDocs){
         log.error(err.message)
       })
     })
-    console.log(`array1 length: "${arr1.length}"`);
-    console.log(`array2 length: "${arr2.length}"`);
+    console.log(`AP array length: "${ap_arr.length}"`);
+    console.log(`BA array length: "${ba_arr.length}"`);
 
     setTimeout(function(){
-      console.log(`${config.color.white} array3 length: "${arr2.length}"`);
-    }, 1000)
-
-
-
+      console.log(`${config.color.white} BA array length: "${ba_arr.length}"`);
+    }, 4000)
 
 }
 
