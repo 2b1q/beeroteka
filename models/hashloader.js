4a7e200cf118cada5122f4df267617a39281fad0
2b3q <2bb3qq@gmail.com>
2017-12-05 09:37:21 +0300

    async delay

--- models/hashloader.js
+++ models/hashloader.js
@@ -8,6 +8,53 @@ var es_client = require('../libs/elasticsearch'), // require ES module
 var ap_arr = [], // apivo search result array from ES
     ba_arr = []; // BeerAdvocate search result array from ES
 
+function resolveAfterDelay(ap_json, ba_json) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(ap_json, ba_json);
+    }, config.es.promiseDelay);
+  });
+}
+
+async function asyncDelay(ap_json, query_object) {
+  es_client.client.search(query.getBaFromAp(query_object))
+    .then(function(resp){
+      if( resp.hits.hits.length > 0 ) {
+        // console.log(config.color.yellow+JSON.stringify(resp.hits.hits[0]));
+        ba_json.ba_score =       resp.hits.hits[0]._score; // Do not use iterator [i] => caz first object is most relevanted response
+        ba_json.ba_beer_score =  resp.hits.hits[0]._source.score;
+        ba_json.ba_img =         resp.hits.hits[0]._source.img;
+        ba_json.ba_title =       resp.hits.hits[0]._source.title;
+        ba_json.ba_url =         resp.hits.hits[0]._source.url;
+        ba_json.ba_ratings =     resp.hits.hits[0]._source.Ratings;
+        ba_json.ba_reviews =     resp.hits.hits[0]._source.Reviews;
+        ba_json.ba_abv =         resp.hits.hits[0]._source.abv;
+        ba_json.ba_brewary =     resp.hits.hits[0]._source.brewary;
+        ba_json.ba_beer =        resp.hits.hits[0]._source.beer;
+        ba_json.ba_style =       resp.hits.hits[0]._source.style;
+        ba_json.ba_category =    resp.hits.hits[0]._source.category;
+
+        ba_arr.push(ba_json);
+        /*
+        INSERTING hashToLoad
+        */
+        // console.log(`${config.color.yellow}BA JSON: ${JSON.stringify(ba_json)}`);
+        // Create an instance of model apivoModel
+        // var hashToLoad_instance = new apivoModel(hashToLoad2);
+        // // Save hashToLoad_instance, passing a callback
+        // hashToLoad_instance.save(function (err) {
+        //   if (err) log.error(`ERROR while INSERTING data: "${err}"`);
+        //   // saved!
+        // });
+      } else {
+        ba_json = {}
+      }
+    }, function(err){
+      log.error(err.message)
+    })
+  return await resolveAfterDelay(ap_json, ba_json);
+}
+
 apivoModel.find({}, function(err, docs){
   if(err) log.error(`ERROR while getting docs from mongo: "${err}"`)
   console.log(docs);
@@ -38,7 +85,7 @@ function getApDocs(){
 
 // searh Matches in BA index
 function searchBaMatches(ApDocs){
-  ApDocs.forEach(function(item){
+  ApDocs.forEach(function(item,i){
     // Create AP properties
     ap_json.ap_beer = item._source.beer || '';
     ap_json.ap_orig_beer_name = item._source['Название'];
@@ -57,8 +104,8 @@ function searchBaMatches(ApDocs){
     ap_json.ap_url = item._source['url'] || '';
     ap_json.ap_taste = item._source['Вкусовые оттенки'] || '';
     ap_json.ap_desc = item._source['desc'] || {};
-    console.log(`${config.color.white}APIVO JSON: ${JSON.stringify(ap_json)}`);
-    ap_arr.push(ap_json);
+    // console.log(`${config.color.white}APIVO JSON: ${JSON.stringify(ap_json)}`);
+    // ap_arr.push(ap_json);
 
     // create BA query object
     let query_object = {
@@ -69,48 +116,26 @@ function searchBaMatches(ApDocs){
       country: ap_json.country_obj.name,
       abv: (isInteger(ap_json.ap_abv) || isFloat(ap_json.ap_abv)) ? ap_json.ap_abv : 0
     }
+
+    // Object.assign(target, ...sources)
+    var obj = {};
+    asyncDelay(ap_json, query_object);
+
+    console.log(`${config.color.white}APIVO JSON: ${JSON.stringify(ap_json)}`);
+    console.log(`${config.color.yellow}BA JSON: ${JSON.stringify(ba_json)}`);
     // console.log(config.color.white+JSON.stringify(query_object));
     // LookUP beer in BA from AP properties
-    es_client.client.search(query.getBaFromAp(query_object))
-      .then(function(resp){
-        if( resp.hits.hits.length > 0 ) {
-          // console.log(config.color.yellow+JSON.stringify(resp.hits.hits[0]));
-          ba_json.ba_score =       resp.hits.hits[0]._score; // Do not use iterator [i] => caz first object is most relevanted response
-          ba_json.ba_beer_score =  resp.hits.hits[0]._source.score;
-          ba_json.ba_img =         resp.hits.hits[0]._source.img;
-          ba_json.ba_title =       resp.hits.hits[0]._source.title;
-          ba_json.ba_url =         resp.hits.hits[0]._source.url;
-          ba_json.ba_ratings =     resp.hits.hits[0]._source.Ratings;
-          ba_json.ba_reviews =     resp.hits.hits[0]._source.Reviews;
-          ba_json.ba_abv =         resp.hits.hits[0]._source.abv;
-          ba_json.ba_brewary =     resp.hits.hits[0]._source.brewary;
-          ba_json.ba_beer =        resp.hits.hits[0]._source.beer;
-          ba_json.ba_style =       resp.hits.hits[0]._source.style;
-          ba_json.ba_category =    resp.hits.hits[0]._source.category;
 
-          ba_arr.push(ba_json);
-          /*
-          INSERTING hashToLoad
-          */
-          console.log(`${config.color.yellow}BA JSON: ${JSON.stringify(ba_json)}`);
-          // Create an instance of model apivoModel
-          // var hashToLoad_instance = new apivoModel(hashToLoad2);
-          // // Save hashToLoad_instance, passing a callback
-          // hashToLoad_instance.save(function (err) {
-          //   if (err) log.error(`ERROR while INSERTING data: "${err}"`);
-          //   // saved!
-          // });
-        }
-      }, function(err){
-        log.error(err.message)
-      })
+
+
     })
+
     console.log(`AP array length: "${ap_arr.length}"`);
     console.log(`BA array length: "${ba_arr.length}"`);
 
-    setTimeout(function(){
-      console.log(`${config.color.white} BA array length: "${ba_arr.length}"`);
-    }, 4000)
+    // setTimeout(function(){
+    //   console.log(`${config.color.white} BA array length: "${ba_arr.length}"`);
+    // }, 4000)
 
 }
 
