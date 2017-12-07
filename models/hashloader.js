8b5ee0e162e75d388a595fc5e6cefc3c527d650b
root <root@beeroteka.ru>
2017-12-07 14:39:24 +0300

    wtf

--- models/hashloader.js
+++ models/hashloader.js
@@ -7,8 +7,7 @@ var es_client = require('../libs/elasticsearch'), // require ES module
 
 var ap_arr = [], // apivo search result array from ES
     ba_arr = [], // BeerAdvocate search result array from ES
-    result_arr = [],
-    allApDocs = [];
+    result_arr = [];
 
 var ap_json = {}, // One matched AP JSON result from ES to LOAD
     ba_json = {}; // One matched BA JSON result from ES to LOAD
@@ -18,7 +17,8 @@ var isFloat = n => n === +n && n !== (n|0),
 
 
 // baQuery return Promise
-function baQuery(q) {
+function baQuery(q,item) {
+  var apjs = item._source
   return new Promise(function(resolve, reject){
     es_client.client.search(query.getBaFromAp(q))
     .then(function(resp){
@@ -36,9 +36,9 @@ function baQuery(q) {
         ba_json.ba_beer =        resp.hits.hits[0]._source.beer;
         ba_json.ba_style =       resp.hits.hits[0]._source.style;
         ba_json.ba_category =    resp.hits.hits[0]._source.category;
-        resolve(ba_json)
+        resolve(apjs, ba_json)
       } else {
-        resolve({})
+        resolve(apjs)
       }
     }, function(err){
       reject(err.message)
@@ -46,14 +46,42 @@ function baQuery(q) {
   })
 }
 
-async function awaitBaResponse(query_obj, i, item){
-  let obj = await baQuery(query_obj)
-  console.log(`${i} AP_JSON\n ${config.color.white}${JSON.stringify(item)}`);
-  console.log(`${config.color.yellow}Query object ${JSON.stringify(query_obj)}`);
+async function awaitBaResponse(item){
+  // Create AP properties
+  ap_json.ap_beer = item._source.beer || '';
+  ap_json.ap_orig_beer_name = item._source['Название'];
+  ap_json.ap_brewary = item._source.brewary || '';
+  ap_json.ap_style = item._source['Вид пива'] || '';
+  ap_json.ap_country = item._source['Страна'];
+  ap_json.country_obj = dict.getCountry(item._source['Страна']) || {};
+  ap_json.ap_abv = item._source.abv;
+  ap_json.ap_vol = item._source['Объем'] || '';
+  ap_json.ap_density = item._source['density'] || '';
+  ap_json.ap_tara = item._source['Тара'] || '';
+  ap_json.ap_type = item._source['Тип брожения'] || '';
+  ap_json.ap_price_str = item._source['Цена'] || '';
+  ap_json.ap_price_num = item._source['price'] || '';
+  ap_json.ap_composition = item._source['Состав'] || '';
+  ap_json.ap_url = item._source['url'] || '';
+  ap_json.ap_taste = item._source['Вкусовые оттенки'] || '';
+  ap_json.ap_desc = item._source['desc'] || {};
+  // create BA query object
+  let query_object = {
+    beer: ap_json.ap_beer,
+    beer_orig_name: ap_json.ap_orig_beer_name,
+    brewary: ap_json.ap_brewary,
+    style: ap_json.ap_style.replace(/[^a-zA-Z0-9 ]/g, ''),
+    country: ap_json.country_obj.name,
+    abv: (isInteger(ap_json.ap_abv) || isFloat(ap_json.ap_abv)) ? ap_json.ap_abv : 0
+  }
+
+  console.log(`${config.color.yellow}Query object ${JSON.stringify(query_object)}`);
+  result_arr.push(await baQuery(query_object, item))
+  console.log(`AP_JSON\n ${config.color.white}${JSON.stringify(item)}`);
   console.log(`${config.color.cyan}Result OBJ: ${JSON.stringify(obj)}`);
   // result_arr.push(await baQuery(query_obj, ap_js))
   // console.log(`${config.color.yellow}APBA JSON\n ${JSON.stringify(result_arr[i])}`);
-  return result_arr[i]
+  return result_arr
 }
 
 apivoModel.find({}, function(err, docs){
@@ -75,39 +103,9 @@ function getApDocs(){
 
 // searh Matches in BA index
 function searchBaMatches(ApDocs){
-  // for(let i=0; i<ApDocs.length; i++){
-  ApDocs.forEach(function(item,i){
-    // Create AP properties
-    ap_json.ap_beer = ApDocs[i]._source.beer || '';
-    ap_json.ap_orig_beer_name = ApDocs[i]._source['Название'];
-    ap_json.ap_brewary = ApDocs[i]._source.brewary || '';
-    ap_json.ap_style = ApDocs[i]._source['Вид пива'] || '';
-    ap_json.ap_country = ApDocs[i]._source['Страна'];
-    ap_json.country_obj = dict.getCountry(ApDocs[i]._source['Страна']) || {};
-    ap_json.ap_abv = ApDocs[i]._source.abv;
-    ap_json.ap_vol = ApDocs[i]._source['Объем'] || '';
-    ap_json.ap_density = ApDocs[i]._source['density'] || '';
-    ap_json.ap_tara = ApDocs[i]._source['Тара'] || '';
-    ap_json.ap_type = ApDocs[i]._source['Тип брожения'] || '';
-    ap_json.ap_price_str = ApDocs[i]._source['Цена'] || '';
-    ap_json.ap_price_num = ApDocs[i]._source['price'] || '';
-    ap_json.ap_composition = ApDocs[i]._source['Состав'] || '';
-    ap_json.ap_url = ApDocs[i]._source['url'] || '';
-    ap_json.ap_taste = ApDocs[i]._source['Вкусовые оттенки'] || '';
-    ap_json.ap_desc = ApDocs[i]._source['desc'] || {};
-    // console.log(`${config.color.white}APIVO JSON: ${JSON.stringify(ap_json)}`);
-    // ap_arr.push(ap_json);
-
-    // create BA query object
-    let query_object = {
-      beer: ap_json.ap_beer,
-      beer_orig_name: ap_json.ap_orig_beer_name,
-      brewary: ap_json.ap_brewary,
-      style: ap_json.ap_style.replace(/[^a-zA-Z0-9 ]/g, ''),
-      country: ap_json.country_obj.name,
-      abv: (isInteger(ap_json.ap_abv) || isFloat(ap_json.ap_abv)) ? ap_json.ap_abv : 0,
-    }
-
+  ApDocs.forEach(function(item){
+    // create object in async await on promise resolve
+    awaitBaResponse(item)
     // Object.assign(target, ...sources)
     // var result_arr = [];
     //
@@ -122,7 +120,6 @@ function searchBaMatches(ApDocs){
     // console.log(config.color.white+JSON.stringify(query_object));
     // LookUP beer in BA from AP properties
     // console.log(`AP object \n${config.color.white}${JSON.stringify(ap_json)}`);
-    awaitBaResponse(query_object, i, item)
 
   })
 
@@ -136,7 +133,7 @@ function searchBaMatches(ApDocs){
   //   result_arr.forEach(function(item){
   //     console.log(`${config.color.cyan} result data  AFTER delay: "${JSON.stringify(item)}"`);
   //   })
-  // }, 5000)
+  // }, 2000)
 
 }
 
