5776c1891e476737f8b70742a02b7556d935b42d
2b3q <2bb3qq@gmail.com>
2018-03-28 09:30:32 +0300

    * fix accordion UI graphics + add styles aggs from ES & mongo (graphics controller)

--- controllers/graphics.js
+++ controllers/graphics.js
@@ -12,8 +12,12 @@ exports.show = function (req, res) {
   res.render('infographics', { title: 'Пиво в цифрах' });
 }
 
-// get data for chart1 (Ales vs Lagers) and return Promise with data
-function chart1() {
+/*
+get data for chart1 (Ales)
+from MongoDB baModel collection
+and return Promise with data
+*/
+function alesMongo() {
   var pattern =
   [
     {
@@ -23,6 +27,7 @@ function chart1() {
           { ba_style: /apa/ig },
           { ba_style: /ipa/ig },
           { ba_style: /stout/ig },
+          { ba_category: /ales/ig }
         ]
       }
     },
@@ -41,17 +46,113 @@ function chart1() {
   });
 }
 
+/*
+get data for chart1 (Lagers)
+from MongoDB baModel collection
+and return Promise with data
+*/
+function lagersMongo() {
+  var pattern =
+  [
+    {
+      $match: {
+        $or: [
+          { ba_style: /lager/ig },
+          { ba_style: /pils/ig },
+          { ba_style: /bock/ig },
+          { ba_category: /lager/ig },
+          { ba_category: /bock/ig },
+          { ba_category: /pils/ig }
+        ]
+      }
+    },
+    {
+      $group: { _id: null, count: {$sum: 1} }
+      // $group: { _id: '$ba_style', count: {$sum: 1} }
+    }
+  ];
+  return new Promise(function(resolve, reject) {
+    baModel
+    .aggregate(pattern)
+    .exec((err, data) => {
+      if(err) reject(err);
+      resolve(data);
+    })
+  });
+}
+
+/*
+get data for chart1 (Hybrid)
+from MongoDB baModel collection
+and return Promise with data
+*/
+function hybridMongo() {
+  var pattern =
+  [
+    {
+      $match: {
+        $or: [
+          { ba_style: /Hybrid/ig },
+          { ba_style: /Vegetable/ig },
+          { ba_style: /fruit/ig },
+          { ba_style: /Herbed/ig },
+          { ba_style: /Spiced/ig },
+          { ba_style: /smoked/ig },
+          { ba_style: /bock/ig },
+          { ba_style: /pils/ig },
+          { ba_category: /Hybrid/ig },
+          { ba_category: /Vegetable/ig },
+          { ba_category: /fruit/ig },
+          { ba_category: /Herbed/ig },
+          { ba_category: /Spiced/ig },
+          { ba_category: /smoked/ig },
+          { ba_category: /bock/ig },
+          { ba_category: /pils/ig }
+        ]
+      }
+    },
+    {
+      $group: { _id: null, count: {$sum: 1} }
+      // $group: { _id: '$ba_style', count: {$sum: 1} }
+    }
+  ];
+  return new Promise(function(resolve, reject) {
+    baModel
+    .aggregate(pattern)
+    .exec((err, data) => {
+      if(err) reject(err);
+      resolve(data);
+    })
+  });
+}
+
+/*
+get data for chart1 (Ales vs Lagers)
+from ES ba index
+and return Promise with data
+*/
+function allStylesES() {
+  return new Promise(function(resolve, reject) {
+    elastic.getAllStyles(styles => {
+      resolve(styles)
+    })
+  })
+}
+
+
 // AllCharts
 function AllCharts(res) {
-  var p1 = chart1(),
-      p2 = Promise.resolve({ c2: 'c2 data' }),
-      p3 = Promise.resolve({ c3: 'c3 data' });
-  Promise.all([ p1, p2, p3 ])
+  var p1 = alesMongo(),
+      p2 = lagersMongo(),
+      p3 = hybridMongo(),
+      p4 = allStylesES();
+  Promise.all([ p1, p2, p3, p4 ])
     .then(charts => {
       res.json({
         c1: charts[0], // chart1 data
         c2: charts[1], // chart2 data
-        c3: charts[2] // chart3 data
+        c3: charts[2], // chart3 data
+        c3: charts[3] // chart3 data
       });
     })
 }
@@ -67,7 +168,7 @@ exports.charts = function (req, res) {
     let chart = req.body.chart || 'all'; // get chart param. default => 'all'
     switch (chart) {
       case 'c1':
-        chart1().then( result => {
+        alesMongo().then( result => {
           res.json(result)
         })
         break;
