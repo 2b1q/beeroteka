4da08cc741a42b74ed4e478043bf25055e58f927
bbq <b-b-q@ya.ru>
2018-04-02 13:15:12 +0300

    optimizing client XHR requests

--- public/js/graphics.js
+++ public/js/graphics.js
@@ -16,53 +16,7 @@ $(function () {
   ctx3.after('<img id="spinner3" src="../images/spinner2.gif" width="50%" height="50%">'); // add spinner
 
   /*
-    get data for chart1
-    ajax POST XHR
-   */
-  var request = $.ajax({
-    url:url,
-    type: 'POST',
-    data: JSON.stringify({ "chart": "c1" }),
-    contentType: 'application/json; charset=utf-8',
-    dataType: 'json'});
-  request.fail(function (err, msg) {
-    $('#spinner1').remove(); // remove beer spinner
-    // add Error alert
-    $('#chart1').after('<div class="alert alert-danger"><strong>Error!</strong><br>Ошибка получения данных для графика 1.<br>HTTP Status: '+err.status+' '+err.statusText+'</div>');
-    console.log('Request failed.\nStatus:'+err.status+'\nStatus text: '+err.statusText+'\nError message: '+msg);
-  });
-  request.done(function(response) {
-    $('#spinner1').remove(); // remove beer spinner
-    ctx1.removeClass('hidden'); // unhide chart1
-    console.log('Chart 1 response: '+JSON.stringify(response, null,2));
-    // construct data for chart1
-    var data = {
-      datasets: [{
-        data: [response.ales, response.lagers, response.hybrid],
-        backgroundColor: [
-          'rgba(204,102,0,1)',
-          'rgba(255,230,153,1)',
-          'rgba(221,153,255,1)'
-        ],
-        borderColor: [
-          'rgba(255,255,255,1)',
-          'rgba(255,255,255,1)',
-          'rgba(255,255,255,1)'
-        ],
-        borderWidth: 6
-      }],
-      // These labels appear in the legend and in the tooltips when hovering different arcs
-      labels: [ 'Ales' , 'Lagers', 'Hybrid' ]
-    };
-    // render chart1 doughnut
-    new Chart(ctx1, {
-        type: 'doughnut',
-        data: data,
-    });
-  });
-
-  /*
-    get data for chart2 (bar)
+    get data for chart1 & chart2
     ajax POST XHR
    */
   var request = $.ajax({
@@ -72,25 +26,52 @@ $(function () {
     contentType: 'application/json; charset=utf-8',
     dataType: 'json'});
   request.fail(function (err, msg) {
+    $('#spinner1').remove(); // remove beer spinner
     $('#spinner2').remove(); // remove beer spinner
     // add Error alert
+    $('#chart1').after('<div class="alert alert-danger"><strong>Error!</strong><br>Ошибка получения данных для графика 1 .<br>HTTP Status: '+err.status+' '+err.statusText+'</div>');
     $('#chart2').after('<div class="alert alert-danger"><strong>Error!</strong><br>Ошибка получения данных для графика 2.<br>HTTP Status: '+err.status+' '+err.statusText+'</div>');
     console.log('Request failed.\nStatus:'+err.status+'\nStatus text: '+err.statusText+'\nError message: '+msg);
+    // add Error alert
   });
   request.done(function(response) {
+    $('#spinner1').remove(); // remove beer spinner
     $('#spinner2').remove(); // remove beer spinner
-    console.log('Chart 2 response: '+JSON.stringify(response, null,2));
+    console.log('Chart 1 and 2 response: '+JSON.stringify(response, null,2));
     // if no data => add info alert
     if( response.ales.length === 0 ||
         response.lagers.length === 0 ||
         response.hybrid.length === 0)
     {
       // add info alert
+      $('#chart1').after('<div class="alert alert-info">Не достаточно данных для построения графика 1.</div>');
       $('#chart2').after('<div class="alert alert-info">Не достаточно данных для построения графика 2.</div>');
-    } else {
+    }
+    else
+    {
+      ctx1.removeClass('hidden'); // unhide chart1
       ctx2.removeClass('hidden'); // unhide chart2
+      // construct data for chart1
+      var data1 = {
+        datasets: [{
+          data: [response.ales[0].count, response.lagers[0].count, response.hybrid[0].count],
+          backgroundColor: [
+            'rgba(204,102,0,1)',
+            'rgba(255,230,153,1)',
+            'rgba(221,153,255,1)'
+          ],
+          borderColor: [
+            'rgba(255,255,255,1)',
+            'rgba(255,255,255,1)',
+            'rgba(255,255,255,1)'
+          ],
+          borderWidth: 6
+        }],
+        // These labels appear in the legend and in the tooltips when hovering different arcs
+        labels: [ 'Ales' , 'Lagers', 'Hybrid' ]
+      };
       // construct data for chart2
-      var data = {
+      var data2 = {
         datasets: [{
           data: [response.ales[0].max_abv, response.lagers[0].max_abv, response.hybrid[0].max_abv],
           label: 'max ABV.',
@@ -108,10 +89,16 @@ $(function () {
         // These labels appear in the legend and in the tooltips when hovering different arcs
         labels: [ 'Ales' , 'Lagers', 'Hybrid' ]
       };
+
+      // render chart1 doughnut
+      new Chart(ctx1, {
+          type: 'doughnut',
+          data: data1,
+      });
       // render chart2 bar
       new Chart(ctx2, {
           type: 'bar',
-          data: data,
+          data: data2,
           options: {
             scales: {
                 xAxes: [{
