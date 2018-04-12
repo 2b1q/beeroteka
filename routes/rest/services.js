53122eb394a1dd74e29a500770558c2529b19d30
bbq <b-b-q@ya.ru>
2018-04-12 18:44:52 +0300

    REST response with async await f() DBGing

--- routes/rest/services.js
+++ routes/rest/services.js
@@ -9,19 +9,29 @@ var catalog_controller = require('../../controllers/catalog');
 exports.attach = function(rest) {
   /** test endpoint */
   // bind the service funciont to only the given http request types
-  rest.assign([ 'get','post' ], // assign incoming HTTP REST methods
-              [ { path: '/test', unprotected: true } ], // config API route
-              test_api ) // setup assync callback service to API route
+  rest.assign([ 'get','post' ],                               // assign incoming HTTP REST methods
+              [ { path: '/test/:wait', unprotected: true },   // path 1
+                { path: '/test', unprotected: true } ],       // path 2
+                test_api )   // setup assync callback service to API route
   /** dataloader endpoint */
   rest.get({ path: '/dataload/:id', unprotected: false }, dataload ); // hashload service /api/dataload/<id>?api_key=<api_key>
 }
 
 // test API
-async function test_api(request, content) {
-	console.log( 'Received headers:' + JSON.stringify( request.headers ) )
+ async function test_api(request, content) {
+  const wait = parseInt(request.parameters.wait); // parseInt return Number
+  if(!wait) return { msg: 'bad query. U need to setup "wait" parametr in "ms"'}
+	// console.log( 'Received headers:' + JSON.stringify( request.headers ) )
 	console.log( 'Received parameters:' + JSON.stringify( request.parameters ) )
 	console.log( 'Received JSON object:' + JSON.stringify( content ) )
-	return { name: 'test name', id: request.parameters.id }
+	// return { name: 'test name', id: request.parameters.id }
+  let response = await (wait => {
+    return new Promise(function(resolve, reject) {
+      setTimeout(function(){ resolve({msg:`hello. This is response after ${wait} ms` }); }, wait);
+    });
+  })(wait)
+  console.log('return response object after await');
+  return response;
 }
 
 // new hashload service
