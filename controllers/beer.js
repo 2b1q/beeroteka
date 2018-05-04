b4ed274f87c9da929dcde22d63bad121594169ca
bbq <b-b-q@ya.ru>
2018-05-04 13:43:22 +0300

    Beer controller: + request logger => singleton pattern

--- controllers/beer.js
+++ controllers/beer.js
@@ -1,39 +1,50 @@
 /** Beer controller
-  - add/delete/lookup beer controller,
-  use beer model
+  - CRUD beer controller
+  - REST API using express middleware
+  - request logger => singleton pattern
+  - async await Promise for waiting long Ops. from model
+  - user data check_module => module pattern
 */
 var config = require('../config/config'),
-    log = require('../libs/log')(module),
-    beer_model = require('../models/beer');
+    log = require('../libs/log')(module);
 
 // AddBeer controller
 async function AddBeer(req, res) {
-  if(check_module.auth(req.query.api_key, res)) res.json({ auth: "OK", api: 'AddBeer' })
+  write.log(req); // write request to log
+  if(check_module.auth(req.query.api_key, res))
+    // exec await Promise AddBeer here => return ID
+    res.json({ auth: "OK", api: 'AddBeer' })
 }
 
 // GetBeer controller
 async function GetBeer(req, res) {
-  if(check_module.chekid(req.query.id, res)) res.json({ msg: `get beer by ID '${check_module.getid()}'`})
-  // exec API here
+  write.log(req);
+  if(check_module.chekid(req.query.id, res))
+    // exec await Promise GetBeer here => return beer data
+    res.json({ msg: `get beer by ID '${check_module.getid()}'`})
 }
 
 
 // DeleteBeer controller
 async function DeleteBeer(req, res) {
+  write.log(req);
   if(check_module.auth(req.query.api_key, res))
     if(check_module.chekid(req.query.id, res))
+      // exec await Promise DeleteBeer here => return status
       res.json({ auth: 'OK', msg: `Delete beer by ID '${check_module.getid()}'`})
 }
 
 // UpdateBeer controller
 async function UpdateBeer(req, res) {
+  write.log(req);
   if(check_module.auth(req.query.api_key, res))
     if(check_module.chekid(req.query.id, res))
+      // exec await Promise UpdateBeer here => return updated beer data
       res.json({ auth: 'OK', msg: `Update beer by ID '${check_module.getid()}'`})
 }
 
 // check module with closures
-var check_module = (function () {
+var check_module = (function() {
   // private data
   let private_id;
   // private functions
@@ -62,13 +73,43 @@ var check_module = (function () {
       else return true
     }
   }
-}());
+})();
 
+// logger singleton
+var logger = (function(){
+  var instance; // keep reference to singleton
+  function initInstance(){
+    // private method
+    function logit(req) {
+      log.info({
+        method: req.method,
+        url:    req.url,
+        ip:     req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+        ua:     req.headers['user-agent']
+      })
+    }
+    return {
+      // public method log()
+      log: req => logit(req)
+    }
+  }
 
+  return {
+    getInstance: function() {
+      if(!instance) {
+        instance = initInstance();
+      }
+      return instance;
+    }
+  }
+})();
+
+// init singleton instance (always refer to same one instance)
+var write = logger.getInstance();
 
 module.exports = {
-  add:      AddBeer,      // add beer
-  get:      GetBeer,      // get beer by ID
-  delete:   DeleteBeer,   // remove beer by ID
-  update:   UpdateBeer    // update beer by ID
+  add:      AddBeer,      // POST   => add beer
+  get:      GetBeer,      // GET    => get beer by ID
+  delete:   DeleteBeer,   // DELETE => remove beer by ID
+  update:   UpdateBeer    // PUT    => update beer by ID
 };
