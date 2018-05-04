f1d5fa5a0f6b2f20853e39b8480b7bd26d66ba92
2b3q <2bb3qq@gmail.com>
2018-05-04 08:38:41 +0300

    refactored:  controller API. Add check module with public interface and closures

--- controllers/beer.js
+++ controllers/beer.js
@@ -8,60 +8,62 @@ var config = require('../config/config'),
 
 // AddBeer controller
 async function AddBeer(req, res) {
-  if(!req.query.api_key) unauth(res, { err: 'unable to set "api_key" param' }) // check api_key param exist
-  else {
-    if(auth(req)) res.json({ auth: "OK", api: 'AddBeer' })
-    else unauth(res, { err: 'bad "api_key"' })
-  }
+  if(check_module.auth(req.query.api_key, res)) res.json({ auth: "OK", api: 'AddBeer' })
 }
 
 // GetBeer controller
 async function GetBeer(req, res) {
-  if(!req.query.id) res.json({ err: `unable to pass ID`}) // check ID param exist
-  else if (!checkID(req)) res.json({ err: `bad ID`}) // check ID param
-  else {
-    let id = parseInt(req.query.id)
-    res.json({ msg: `get beer by ID '${id}'`})
-  }
+  if(check_module.chekid(req.query.id, res)) res.json({ msg: `get beer by ID '${check_module.getid()}'`})
+  // exec API here
 }
 
 
 // DeleteBeer controller
 async function DeleteBeer(req, res) {
-  if(!req.query.api_key) unauth(res, { err: 'unable to set "api_key" param' }) // check api_key param exist
-  else if(!req.query.id) res.json({ err: `unable to pass ID`}) // check ID param exist
-  else if (!checkID(req)) res.json({ err: `bad ID`}) // check ID param
-  else {
-    let id = parseInt(req.query.id);
-    if(auth(req)) res.json({ auth: "OK", api: `DeleteBeer ID: '${id}'` }) // need await promise frome model
-    else unauth(res, { err: 'bad "api_key"' })
-  }
+  if(check_module.auth(req.query.api_key, res))
+    if(check_module.chekid(req.query.id, res))
+      res.json({ auth: 'OK', msg: `Delete beer by ID '${check_module.getid()}'`})
 }
 
 // UpdateBeer controller
 async function UpdateBeer(req, res) {
-  if(!req.query.api_key) unauth(res, { err: 'unable to set "api_key" param' }) // check api_key param exist
-  else if(!req.query.id) res.json({ err: `unable to pass ID`}) // check ID param exist
-  else if (!checkID(req)) res.json({ err: `bad ID`}) // check ID param
-  else {
-    let id = parseInt(req.query.id);
-    if(auth(req)) res.json({ auth: "OK", api: `UpdateBeer ID: '${id}'` }) // need await promise frome model
-    else unauth(res, { err: 'bad "api_key"' })
+  if(check_module.auth(req.query.api_key, res))
+    if(check_module.chekid(req.query.id, res))
+      res.json({ auth: 'OK', msg: `Update beer by ID '${check_module.getid()}'`})
+}
+
+// check module with closures
+var check_module = (function () {
+  // private data
+  let private_id;
+  // private functions
+  let send_response = function(res, msg, status) {
+    res.status(status);
+    res.json(msg)
+  };
+  // check ID is number
+  let checkid = id => isNaN(parseInt(id))? false : true;
+  // check IF token exist
+  let chek_token = token => config.restOptions.apiKeys.includes(token);
+  // public interface
+  return {
+    chekid: function(id, res) {
+      if(!id) send_response(res, { err: 'unable to pass ID'}, 200)
+      else if(!checkid(id)) send_response(res, { err: 'bad ID'}, 500)
+      else {
+        private_id = parseInt(id);
+        return true
+      }
+    },
+    getid: () => private_id,
+    auth: function(api_key, res) {
+      if(!api_key) send_response(res, { err: 'unable to set "api_key" param' }, 401)
+      else if(!chek_token(api_key)) send_response(res, { err: 'bad "api_key"' }, 401)
+      else return true
+    }
   }
-}
+}());
 
-
-// AUTH required
-const unauth = (res, msg) => {
-  res.status(401)
-  res.json(msg)
-}
-
-// check AUTH api_key
-const auth = req => config.restOptions.apiKeys.includes(req.query.api_key)
-
-// check beer ID
-const checkID = req => isNaN(parseInt(req.query.id))? false : true;
 
 
 module.exports = {
